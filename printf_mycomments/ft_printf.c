7193061358c9e8ee2a62b04c3ab1691f5177b495
Alejandro_Rodriguez <alex.rodriguez1003@gmail.com>
2022-09-30 20:47:01 +0200

    first works

--- printf_mycomments/ft_printf.c
+++ printf_mycomments/ft_printf.c
@@ -6,13 +6,25 @@
 /*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 20:23:07 by alejarod          #+#    #+#             */
-/*   Updated: 2022/09/29 22:13:24 by alejarod         ###   ########.fr       */
+/*   Updated: 2022/09/30 20:34:30 by alejarod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h" 
+#include "libftprintf.h"
 
 int	ft_printf(char const *str, ...)
 {
-	
+	va_list	args;		//I am going to save the arguments in this variable
+	unsigned int num_args;
+	//int	i;
+
+	va_start(args, 2);
+
+
+
+	ft_printf("Hello", 1, 2);
+	printf("Hello", 1, 2);
+
+	return(0);
+
 }
--- printf_mycomments/libftprintf.h
+++ printf_mycomments/libftprintf.h
@@ -6,12 +6,15 @@
 /*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 21:15:26 by alejarod          #+#    #+#             */
-/*   Updated: 2022/09/29 21:16:21 by alejarod         ###   ########.fr       */
+/*   Updated: 2022/09/30 20:32:07 by alejarod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef
-
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <stdio.h>
-# include <stdarg.h>	// // Añadir la librería para la gestión de las variádricas
+# include <stdarg.h>	// Añadir la librería para la gestión de las variádicas
+# include <unistd.h>
+
+#endif
